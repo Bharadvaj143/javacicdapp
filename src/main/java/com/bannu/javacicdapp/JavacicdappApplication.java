712689b7ec97bb06b9a861f65748300aa5c9a8d2
Bannu <bharadvajnanepalli7@gmail.com>
2026-03-02 23:48:56 +0530

    Initial commit for the javacicdapp

---
+++ src/main/java/com/bannu/javacicdapp/JavacicdappApplication.java
@@ -0,0 +1,13 @@
+package com.bannu.javacicdapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavacicdappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavacicdappApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/bannu/javacicdapp/helloController.java
@@ -0,0 +1,20 @@
+package com.bannu.javacicdapp;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+
+@RestController
+public class helloController {
+    @GetMapping("/hello")
+    public String sayHello() {
+        return "Hello, World!";
+    }
+    @GetMapping("/greet")
+    public String greet(String name) {
+        return "Hello, " + name + "!";
+    }
+
+    
+}
